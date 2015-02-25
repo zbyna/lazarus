zbyna/lazarus

commit 2b1bcc13a2497ecf39e03b515e83953ad057cfa0

TCheckListBox: when user presses spacebar: - if ItemIndex = -1: set it to 0 and fire OnClick; - fire OnClickCheck. Issue #0024695.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@47992 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/checklst.pas
@@ -299,12 +299,23 @@ procedure TCustomCheckListBox.KeyDown(var Key: Word; Shift: TShiftState);
 var
   Index: Integer;
 begin
-  if (Key = VK_SPACE) and (Shift=[]) and (ItemIndex >= 0) and ItemEnabled[ItemIndex] then
+  if (Key = VK_SPACE) and (Shift=[]) then
   begin
-    Index := ItemIndex;
-    Checked[Index] := not Checked[Index];
-    ItemClick(Index);
-    Key := VK_UNKNOWN;
+    //Delphi (7) sets ItemIndex to 0 in this case and fires OnClick
+    if (ItemIndex < 0) and (Items.Count >= 0) then
+    begin
+      ItemIndex := 0;
+      Click;
+    end;
+    if (ItemIndex >= 0) and ItemEnabled[ItemIndex] then
+    begin
+      Index := ItemIndex;
+      Checked[Index] := not Checked[Index];
+      ClickCheck;
+      //ToDo: does Delphi fire OnItemClick and in the same order?
+      ItemClick(Index);
+      Key := VK_UNKNOWN;
+    end;
   end else
     inherited KeyDown(Key,Shift);
 end;

commit 06de6576becb9bc0675193aa4f248b835e1e8733

TCheckListBox: fix possible crash introduced in r47992.
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@47993 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/checklst.pas
@@ -302,7 +302,7 @@ begin
   if (Key = VK_SPACE) and (Shift=[]) then
   begin
     //Delphi (7) sets ItemIndex to 0 in this case and fires OnClick
-    if (ItemIndex < 0) and (Items.Count >= 0) then
+    if (ItemIndex < 0) and (Items.Count > 0) then
     begin
       ItemIndex := 0;
       Click;
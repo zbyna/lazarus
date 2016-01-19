zbyna/lazarus

commit 382bebe4a50792f31b230170a6bda2328f944174

IDEIntf, AnchorDocking: make ResetSplitters public and use it in TMainIDEBar.InitPaletteAndCoolBar. Related to issue 28096
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51338 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/mainbar.pas
@@ -674,6 +674,7 @@ begin
   RefreshCoolbar;
   ComponentPageControl.OnChange(Self);//refresh component palette with button reposition
   SetMainIDEHeight;
+  IDEDockMaster.ResetSplitters;
 end;
 
 procedure TMainIDEBar.RefreshCoolbar;

components/anchordocking/anchordocking.pas
@@ -526,7 +526,6 @@ type
     procedure MapTreeToControls(Tree: TAnchorDockLayoutTree);
     function RestoreLayout(Tree: TAnchorDockLayoutTree; Scale: boolean): boolean;
     procedure EnableAllAutoSizing;
-    procedure ResetSplitters;
     procedure ClearLayoutProperties(AControl: TControl; NewAlign: TAlign = alClient);
     procedure PopupMenuPopup(Sender: TObject);
     procedure ChangeLockButtonClick(Sender: TObject);
@@ -614,6 +613,7 @@ type
     procedure LoadSettings(Settings: TAnchorDockSettings);
     procedure SaveSettings(Settings: TAnchorDockSettings);
     function SettingsAreEqual(Settings: TAnchorDockSettings): boolean;
+    procedure ResetSplitters;
 
     // manual docking
     procedure ManualFloat(AControl: TControl);

components/anchordocking/design/registeranchordocking.pas
@@ -82,6 +82,7 @@ type
     procedure AdjustMainIDEWindowHeight(const AIDEWindow: TCustomForm;
       const AAdjustHeight: Boolean; const ANewHeight: Integer); override;
     procedure CloseAll; override;
+    procedure ResetSplitters; override;
   end;
 
   { TAnchorDockIDEFrame }
@@ -230,6 +231,11 @@ begin
     aManager:=TAnchorDockManager(AForm.DockManager);
     aManager.PreferredSiteSizeAsSiteMinimum:=false;
   end;
+end;
+
+procedure TIDEAnchorDockMaster.ResetSplitters;
+begin
+  DockMaster.ResetSplitters;
 end;
 
 procedure TIDEAnchorDockMaster.MakeIDEWindowDockable(AControl: TWinControl);

components/ideintf/idewindowintf.pas
@@ -476,6 +476,7 @@ type
     procedure AdjustMainIDEWindowHeight(const {%H-}AIDEWindow: TCustomForm;
       const {%H-}AAdjustHeight: Boolean; const {%H-}ANewHeight: Integer); virtual;
     procedure CloseAll; virtual; // close all forms, called after IDE has saved all and shuts down
+    procedure ResetSplitters; virtual; abstract; // if the dock site has been resized after loading, you have to reset (percentual) splitters
     function DockedDesktopOptClass: TAbstractDesktopDockingOptClass; virtual; abstract;
     property HideSimpleLayoutOptions: boolean read FHideSimpleLayoutOptions;
   end;

commit 16a53aceba39f4cddb24ce9fda5834f909fc7e70

IDE: fix InitPaletteAndCoolBar for non-docked IDE. Regression from r51338. Issue 28096
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@51346 4005530d-fff6-0310-9dd1-cebe43e6787f

ide/mainbar.pas
@@ -674,7 +674,8 @@ begin
   RefreshCoolbar;
   ComponentPageControl.OnChange(Self);//refresh component palette with button reposition
   SetMainIDEHeight;
-  IDEDockMaster.ResetSplitters;
+  if IDEDockMaster<>nil then
+    IDEDockMaster.ResetSplitters;
 end;
 
 procedure TMainIDEBar.RefreshCoolbar;